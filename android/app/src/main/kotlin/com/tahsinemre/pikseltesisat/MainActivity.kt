0a99355ff8986f771bee79c53c992aabe4134c9c
Tahsin Emre <telli@tahsinemre.com>
2024-11-08 22:34:53 +0300

    init

---
+++ android/app/src/main/kotlin/com/tahsinemre/pikseltesisat/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.tahsinemre.pikseltesisat
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+import 'package:pikseltesisat/app.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  runApp(const App());
+}

55f1cb87cbcd9f6d507bf4d05312ef5482b68c48
Tahsin Emre <telli@tahsinemre.com>
2024-11-08 23:14:50 +0300

    configure Firebase integration and update localization structure; add GoogleService-Info.plist and google-services.json

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,14 @@
+import 'package:easy_localization/easy_localization.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:pikseltesisat/app.dart';
+import 'package:pikseltesisat/firebase_options.dart';
+import 'package:pikseltesisat/product/init/localization/localization_manager.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  runApp(const App());
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+  await EasyLocalization.ensureInitialized();
+
+  runApp(LocalizationManager.app(const App()));
 }
--- lib/product/init/localization/localization_manager.dart
+++ lib/product/init/localization/localization_manager.dart
@@ -1,6 +1,22 @@
-final class LocalizationManager {
-  LocalizationManager._();
+import 'package:easy_localization/easy_localization.dart';
+import 'package:flutter/material.dart';
 
-  static const String path = '';
-  static const String localePath = 'locale';
+@immutable
+final class LocalizationManager {
+  const LocalizationManager._();
+
+  static EasyLocalization app(Widget app) {
+    return EasyLocalization(
+      path: path,
+      supportedLocales: supportedLocales,
+      useOnlyLangCode: isOnlyLangCode,
+      fallbackLocale: fallbackLocale,
+      child: app,
+    );
+  }
+
+  static const path = 'assets/localization';
+  static const fallbackLocale = Locale('tr');
+  static const supportedLocales = [Locale('tr')];
+  static const isOnlyLangCode = true;
 }

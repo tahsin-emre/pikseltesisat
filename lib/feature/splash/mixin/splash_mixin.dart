d1cd94f55c321674431ded8867bd3b24a3e1162c
Tahsin Emre <telli@tahsinemre.com>
2024-11-09 14:26:16 +0300

    refactor: move customer, plumber, and home views to app_admin feature; add splash view and mixin

---
+++ lib/feature/splash/mixin/splash_mixin.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+import 'package:pikseltesisat/feature/splash/view/splash_view.dart';
+
+mixin SplashMixin on State<SplashView> {
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  void navigateToAdminApp() {}
+  void navigateToPlumberApp() {}
+}
--- lib/product/init/router/app_routes.dart
+++ lib/product/init/router/app_routes.dart
@@ -1,9 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
-import 'package:pikseltesisat/feature/customers/view/customer_list_view.dart';
-import 'package:pikseltesisat/feature/home/view/home_view.dart';
+import 'package:pikseltesisat/feature/app_admin/customers/view/customer_list_view.dart';
+import 'package:pikseltesisat/feature/app_admin/home/view/home_view.dart';
 import 'package:pikseltesisat/feature/main/view/main_view.dart';
-import 'package:pikseltesisat/feature/plumbers/view/plumber_list_view.dart';
+import 'package:pikseltesisat/feature/app_admin/plumbers/view/plumber_list_view.dart';
 import 'package:pikseltesisat/product/init/router/app_router.dart';
 
 part 'routes/main_routes.dart';

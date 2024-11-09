ccc4c59c34df9df17ed90eafa97856edc6081137
Tahsin Emre <telli@tahsinemre.com>
2024-11-10 02:05:45 +0300

    feat: add customer creation and detail views; implement mixins for state management and create initial scaffold for customer views

---
+++ lib/feature/customers/view/customer_create_view.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+final class CustomerCreateView extends StatefulWidget {
+  const CustomerCreateView({super.key});
+
+  @override
+  State<CustomerCreateView> createState() => _CustomerCreateViewState();
+}
+
+class _CustomerCreateViewState extends State<CustomerCreateView> {
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold();
+  }
+}
---
+++ lib/feature/customers/view/customer_detail_view.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+final class CustomerDetailView extends StatefulWidget {
+  const CustomerDetailView({super.key});
+
+  @override
+  State<CustomerDetailView> createState() => _CustomerDetailViewState();
+}
+
+class _CustomerDetailViewState extends State<CustomerDetailView> {
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold();
+  }
+}
